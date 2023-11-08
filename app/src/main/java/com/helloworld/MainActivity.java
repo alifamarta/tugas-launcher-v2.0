bf71fdbdc5c144c2f55be1441b7e9601bf9a6680
Alif_Amarta <alifamarta23@gmail.com>
2023-11-08 08:03:24 +0700

    Update

--- app/src/main/java/com/helloworld/MainActivity.java
+++ app/src/main/java/com/helloworld/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
     private int n2 = 0;
     private TextView mShowCount;
     private EditText mEditText;
-    private int max;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
 
+        int max = Integer.parseInt(mEditText.getText().toString());
 
         if (mCount >= max){
             Toast.makeText(this, R.string.toast_max_message, Toast.LENGTH_SHORT).show();
